48eddcbbae370c4ac048a019d8d8d147fa63ab8c
Vitaly Y. <yarokha@gmail.com>
2024-10-12 14:28:33 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include <filesystem>
 #include <map>
 #include "taped.h"
 #include "sorttape.h"
 
+
 std::map<std::string, int> LoadConfig();
 
-int main(){
+int main() {
     std::map<std::string, int> settings = LoadConfig();
 
-    std::string in_path;
-    std::string out_path;
-    std::getline(std::cin, in_path);
-    std::getline(std::cin, out_path);
+    std::string path;
 
-    Taped input(in_path, std::ios::out, settings);
-    Taped output(out_path, std::ios::out|std::ios::trunc, settings);
-    
+    std::getline(std::cin, path);
+    Taped input(path, std::ios::in, settings);
+
+    std::getline(std::cin, path);
+    Taped output(path, std::ios::out | std::ios::trunc, settings);
+
     tape::sort(input, output, settings);
+
 }
 
 
@@ -32,6 +33,6 @@ std::map<std::string, int> LoadConfig() {
     while (std::getline(config_file, param_name, '='), std::getline(config_file, param_val)) {
         settings[param_name] = std::stoi(param_val);
     }
-    
+
     return settings;
 }
